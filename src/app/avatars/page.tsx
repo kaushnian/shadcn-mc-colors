df93c7612c6b010c844106c426be694940f004a8
Eugene Kaushnian <kefbox@gmail.com>
2024-07-16 10:25:38 -0400

    Add nav

--- src/app/avatars/page.tsx
+++ src/app/avatars/page.tsx
@@ -1,10 +1,15 @@
 'use client';
 import Avatars from '@/components/avatars';
 import ColorPalette from '@/components/color-palette';
+import Link from 'next/link';
 
 export default function AvatarsPage() {
   return (
     <div className="w-[40%] m-auto text-center">
+      <Link href="/" style={{ color: 'blue' }}>
+        Home
+      </Link>
+
       <h1 className="text-2xl my-4">Avatars</h1>
 
       <Avatars></Avatars>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 'use client';
 import ColorPalette from '@/components/color-palette';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
+      <Link href="/avatars" style={{ color: 'blue' }}>
+        Avatars
+      </Link>
+
       <ColorPalette />
     </div>
   );
